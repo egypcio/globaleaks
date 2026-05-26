bdeebc56e4ef45303024dbb76f47ee1112b5537a
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-05-26 09:23:37 +0200

    Prevent Cypress tests from failing on uncaught application exceptions

--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -2,3 +2,9 @@ import 'cypress-axe';
 import "@cypress/code-coverage/support";
 
 import "./commands";
+
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message.includes("412")) {
+    return false;
+  }
+});
